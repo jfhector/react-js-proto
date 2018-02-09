603eae25d946b8703f50332c732839cb0f3f056f
Jean-Francois Hector <jeanfrancois.hector@googlemail.com>
2018-02-09 17:55:28 +0000

    updates to boilerplate following ngi-proto. next step is to redo this boilerplate on create-react-app. all this is working in ngi-protoFeb8

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+
+// export * from './atoms/XXX.js'
+// export * from './molecules/XXX.js'
+// export * from './organisms/XXX.js'
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import styled from 'styled-components'
+import {colors} from './docStyles'
+import {
 
-import { App } from "./App"
+} from './components/'
+
+class Component extends React.Component {
+      constructor(props) {
+            super(props)
+
+            this.state = {
+            }
+      }
+
+      render() {
+            const {
+                  className,
+            } = this.props
+
+            return (
+
+                  <div className={className}>
+                        Test
+                  </div>
+
+            )
+      }
+}
+
+const App = styled(Component)`
+`
 
 ReactDOM.render(<App />, document.getElementById("root"))
